39ba309b6bad130897adbace08bbb2bb56d7bc83
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2021-12-24 15:08:23 +0000

    adding tests again

--- Adeel/Sprint2/AdeeldynamoDB/integtests/integ_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from aws_cdk import core 
-
-from adeeldynamo_db.adeeldynamo_db_stack import  AdeeldynamoDbStack
-def test_ineg():
-    
-    assert 1==1
---
+++ Adeel/Sprint2/AdeeldynamoDB/integtests/lambda_test.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_ineg():
+    
+    assert 1==1
