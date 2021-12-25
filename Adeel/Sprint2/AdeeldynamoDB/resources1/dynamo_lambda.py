67b2f91bddea0d78dd1ed326c6ca25ecba3b6b1f
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2021-12-25 12:14:50 +0000

    debuging

--- Adeel/Sprint2/AdeeldynamoDB/resources1/dynamo_lambda.py
+++ Adeel/Sprint2/AdeeldynamoDB/resources1/dynamo_lambda.py
@@ -8,15 +8,21 @@ client = boto3.client('dynamodb')
   
 def lambda_handler(event, context):
    # db = boto3.resource('dynamodb')
+    tables = ["Beta-adeelStack-BDtable331D4FEF-1VA4X6BOIHL36","Prod-adeelStack-BDtable331D4FEF-1TTSO0FY0P0ZE"]
     client = boto3.client('dynamodb')
     message = event['Records'][0]['Sns']
     msg = json.loads(message['Message'])
+    if msg['AlarmName'][0] == 'P':
+        table_name = tables[1]
+    elif msg['AlarmName'][0] == 'B':
+        table_name = table[0]
+        
     #table_name = db.Table('AdeelAlarm')
-    record = event['Records'][0]['dynamodb']
-    arnText = record['eventSourceARN']
-    listof = arnText.split('/')
-    index = listof.index('stream')
-    table_name = listof[index-1]
+    #record = event['Records'][0]['dynamodb']
+    #arnText = record['eventSourceARN']
+    #listof = arnText.split('/')
+    #index = listof.index('stream')
+    #table_name = listof[index-1]
 
      ############################## Putting values in dynamo table###############################
     #table_name=os.getenv('table_name')
@@ -25,4 +31,5 @@ def lambda_handler(event, context):
     Item={
         'Timestamp':{'S' : message['Timestamp']},
         'Reason':{'S':msg['NewStateReason']}
+        'URL':{'S':data['Trigger']['Dimensions'][0]['value']}
     })
