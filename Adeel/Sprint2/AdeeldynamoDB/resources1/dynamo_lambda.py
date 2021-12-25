ffc5cd9e687313c49179f5a17a83fa84aa326ab5
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2021-12-25 11:28:41 +0000

    debuging

--- Adeel/Sprint2/AdeeldynamoDB/resources1/dynamo_lambda.py
+++ Adeel/Sprint2/AdeeldynamoDB/resources1/dynamo_lambda.py
@@ -12,11 +12,16 @@ def lambda_handler(event, context):
     message = event['Records'][0]['Sns']
     msg = json.loads(message['Message'])
     #table_name = db.Table('AdeelAlarm')
+    record = event['Records'][0]['dynamodb']
+    arnText = record['eventSourceARN']
+    listof = arnText.split('/')
+    index = listof.index('stream')
+    table_name = listof[index-1]
 
      ############################## Putting values in dynamo table###############################
-    table_name=os.getenv('table_name')
+    #table_name=os.getenv('table_name')
     client.put_item(
-    TableName = 'AdeelAdynamo',
+    TableName = table_name,
     Item={
         'Timestamp':{'S' : message['Timestamp']},
         'Reason':{'S':msg['NewStateReason']}
